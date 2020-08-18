dedf94d20ef25236f9e112178cfe0d275948330e
Yoyo San <veselu@gmail.com>
2020-08-18 14:47:49 +0300

    style: Arrange layout, text and links

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,17 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page padding class="column wrap justify-center items-center gap">
+    <div class>
+      <q-avatar size="5rem" color="primary" text-color="white" icon="school" />
+    </div>
+    <div class="q-ma-lg">
+      Check out the course at
+      <a
+        href="https://frontendmasters.com/courses/advanced-css-layouts/"
+        target="_blank"
+      >Frontend Masters</a>
+    </div>
+
+    <div class="col-12 text-center">Use the menu on the left to navigate between course lessons.</div>
   </q-page>
 </template>
 
